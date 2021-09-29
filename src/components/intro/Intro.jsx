d3d0feb7cc279e49d5cde49daed88340c5a10762
Jonathan Dong <jdong6227@gmail.com>
2021-09-29 14:49:25 -0400

    img adjust and added resources

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -5,14 +5,14 @@ export default function Intro() {
     <div className="intro" id="intro">
       <div className="left">
         <div className="imgContainer">
-          <img src="assets/dong.png" alt="me"></img>
+          <img src="assets/dong2.png" alt="me"></img>
         </div>
       </div>
       <div className="right">
         <div className="wrapper">
             <h2>Hi there, I'm</h2>
             <h1>Jonathan Dong</h1>
-            <h3>Freelance<span> </span></h3>
+            <h3>Freelance <span>Software Developer</span></h3>
         </div>
         <a href ="#portfolio">
             <img src = "assets/arrow.png" alt=""></img> 
