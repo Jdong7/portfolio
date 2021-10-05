4d16452c595641fa75e7f05b27875963f74a1e83
Jonathan Dong <jdong6227@gmail.com>
2021-10-05 13:30:19 -0400

    finished intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,6 +1,20 @@
 import "./intro.scss";
+import { init } from 'ityped'
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+
+  const textRef = useRef();
+
+  useEffect(() => {
+      init(textRef.current, {
+        showCursor: true,
+        backDelay: 1500,
+        backSpeed: 60,
+        strings: ["Developer", "Designer"],
+
+      });
+  }, []);
   return (
     <div className="intro" id="intro">
       <div className="left">
@@ -12,7 +26,7 @@ export default function Intro() {
         <div className="wrapper">
             <h2>Hi there, I'm</h2>
             <h1>Jonathan Dong</h1>
-            <h3>Freelance <span>Software Developer</span></h3>
+            <h3>Software <span ref={textRef}></span></h3>
         </div>
         <a href ="#portfolio">
             <img src = "assets/arrow.png" alt=""></img> 
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,6 +3,20 @@ import "./portfolio.scss"
 export default function Portfolio() {
     return (
         <div className = "portfolio" id = "portfolio">
+            <h1>Portfolio</h1>
+            <ul>
+                <li className="active">Featured</li>
+                <li>Web App</li>
+                <li>Mobile App</li>
+                <li>Game Design</li>
+
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="" alt=""/>
+                    <h3>banking</h3>
+                </div>
+            </div>
             
         </div>
     )
