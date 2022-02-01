e25cf85c15af6886a14bbd79c0e2f8d41296b405
Jonathan Dong <jdong6227@gmail.com>
2022-01-31 19:07:35 -0500

    fixed package-lock.json

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,20 +4,6 @@ export default function Portfolio() {
     return (
         <div className = "portfolio" id = "portfolio">
             <h1>Portfolio</h1>
-            <ul>
-                <li className="active">Featured</li>
-                <li>Web App</li>
-                <li>Mobile App</li>
-                <li>Game Design</li>
-
-            </ul>
-            <div className="container">
-                <div className="item">
-                    <img src="" alt=""/>
-                    <h3>banking</h3>
-                </div>
-            </div>
-            
         </div>
     )
 }
